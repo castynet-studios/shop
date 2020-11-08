f1f50176153a8fc71e5d32a28ac0b141bf1fb9e3
castynet <riungemaina@gmail.com>
2020-11-08 10:14:29 +0300

    App: imported navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Products from "./components/products"
+import Nav from "./components/nav"
 // import { useApi } from "./components/productsContext"
 
 function App() {
   return (
     <>
+      <Nav/>
       <Products/>
     </>
   );
