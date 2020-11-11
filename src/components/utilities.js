1e87f7ae312c1aea17d238fc12c52a5d92957c18
castynet <riungemaina@gmail.com>
2020-11-11 11:00:36 +0300

    components/utilities: created & added existing layouts

---
+++ src/components/utilities.js
@@ -0,0 +1,27 @@
+import { RadioCircleMarked } from "@styled-icons/boxicons-regular/RadioCircleMarked";
+import { ExternalLink } from "@styled-icons/heroicons-outline/ExternalLink";
+import { BoltCircle } from "@styled-icons/boxicons-regular/BoltCircle";
+import { CheckCircle } from "@styled-icons/boxicons-regular/CheckCircle"; 
+
+
+export const Icons = {
+    DotCircle: RadioCircleMarked,
+    ExternalLink: ExternalLink,
+    Bolt: BoltCircle,
+    Check: CheckCircle,
+}
+
+export const Quotes = [
+    "Live for today, plan for tomorrow, party tonight 🎉",
+    "Live to work, work to party, party to live 🎉",
+    "If I stay home one night I start spreading rumours to my dogs 🐶",
+    "Empowering Software ⚡",
+    "The Generation that is tech 🌍",
+    "Treat yo’ self 🎁",
+    "Happiness is not in money, but in shopping 🛒",
+    "Anyone who lives within their means suffers from a lack of imagination 📦",
+    "You can always find something you want 💝",
+    "We all need some good retail therapy 🍀",
+    "Exercise? oh i thought you said accessorize 🎮",
+    "Time moves too fast for an analog watch 🛸",
+]
