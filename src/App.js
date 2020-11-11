175f8b42b849beb98238c70730a81ab87a21b253
castynet <riungemaina@gmail.com>
2020-11-11 11:01:23 +0300

    App: imported quotes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Products from "./components/products";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
+import Quotes from "./components/quotes"
 // import { useApi } from "./components/productsContext"
 
 function App() {
   return (
     <>
-      <Nav/>
+      <Nav />
+      <Quotes/>
       <Products />
       <Footer/>
     </>
