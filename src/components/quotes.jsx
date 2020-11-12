7ea8abf9112a85cfd46f146e64be94c737178888
castynet <riungemaina@gmail.com>
2020-11-12 12:19:12 +0300

    components/quotes: added insight icons

--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -1,21 +1,77 @@
 import React from "react";
 import styled from "styled-components";
-import { Quotes } from "./utilities";
+import { Quotes, Icons } from "./utilities";
 
 export default function QuotesSection() {
   var singleQuote = Quotes[Math.floor(Math.random() * Quotes.length)];
 
   return (
     <>
-      <Quote>{singleQuote}</Quote>
+      <QuoteWrap>
+        <Quote> {singleQuote}</Quote>
+        <Insight>
+          <p>Some Insight;</p>
+          <Flame />
+          {"  "}Best Value,{"  "}
+          <Heart />
+          {"  "}Good Value,{"  "}
+          <Check />
+          {"  "}Works as Expected
+        </Insight>
+      </QuoteWrap>
     </>
   );
 }
-const Quote = styled.p`
+
+const Flame = styled(Icons.Flame)`
+  vertical-align: text-bottom;
+  height: 25px;
+  color: #bc00dc;
+  background-color: #000;
+  background-color: rgb(96 125 139 / 0.1);
+  border-radius: 3px;
+  box-shadow: 0px 2px 1px -1px rgba(56, 40, 40, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Check = styled(Icons.Check)`
+  vertical-align: text-bottom;
+  height: 25px;
+  color: #008000;
+  background-color: rgb(96 125 139 / 0.1);
+  border-radius: 3px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Heart = styled(Icons.Heart)`
+  vertical-align: text-bottom;
+  height: 25px;
+  color: #ff0057;
+  background-color: rgb(96 125 139 / 0.1);
+  border-radius: 3px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Insight = styled.p`
+  background-color: #fff;
+  padding: 12px 20px;
+
+  p {
+    padding-bottom: 10px;
+    text-decoration: underline;
+  }
+`;
+
+const Quote = styled.div`
+  padding: 12px 20px;
+`;
+
+const QuoteWrap = styled.div`
   text-align: center;
   margin: 20px auto;
   background-color: #f8ccff;
-  padding: 12px 20px;
   text-transform: uppercase;
   font-weight: 500;
   letter-spacing: 1px;
