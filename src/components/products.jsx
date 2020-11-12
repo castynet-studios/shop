e584ecd965f16b0834ecf6728c3394a1a3197c79
castynet <riungemaina@gmail.com>
2020-11-12 17:15:27 +0300

    components/products: added condtional rendering for rating & price

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -5,27 +5,56 @@ import { Icons } from "./utilities";
 
 export default function Products() {
   const products = useApi().products;
-  console.log(useApi().products);
+  console.log(products);
+
+  function insight(rating) {
+    var r = parseFloat(rating);
+    if (r > 8.5) {
+      return <Flame />;
+    } else if (r > 7.5) {
+      return <Heart />;
+    } else if (r > 5.5) {
+      return <Check />;
+    } else {
+      return;
+    }
+  }
+
+  function findPrice(Jumia, Kilimall, Other) {
+    var JumiaP = parseFloat(Jumia.replace(/,/g, ""));
+    var KilimallP = parseFloat(Kilimall.replace(/,/g, ""));
+    var OtherP = parseFloat(Other.replace(/,/g, ""));
+    console.log(JumiaP, KilimallP, OtherP);
+    return Math.min(JumiaP, KilimallP, OtherP);
+  }
 
   return (
     <>
       <Page>
         {products.map((products) => (
-          <div key={products._id}>
-            <Wrapper>
-              <Image
-                src={"https://shop.api.genztech.xyz" + products.Image.path}
-                alt={products.Description}
-              />
-              <Title>{products.Title}</Title>
-              <Price>KSH {products.JumiaPrice}</Price>
-              <Rate>
-                Buyers Rating {products.BuyersScore}/10
-                <Icons.DotCircle size="30" />
-              </Rate>
-            </Wrapper>
-          </div>
+          <Wrapper key={products._id}>
+            <Image
+              src={"https://shop.api.genztech.xyz" + products.Image.path}
+              alt={products.Description}
+            />
+            <Title>{products.Title}</Title>
+            <Rate color={products.BuyersScore}>
+              {insight(products.BuyersScore)}
+              {"  "} Buyers Score {"  "}
+              <span className="coloured">{products.BuyersScore}</span>
+              /10
+            </Rate>
+            <Price>
+              KSH{" "}
+              {findPrice(
+                products.JumiaPrice,
+                products.KilimallPrice,
+                products.OtherPrice
+              )}
+            </Price>
+          </Wrapper>
         ))}
+        <div style={{ width: "220px", height: "0" }}></div>
         <div style={{ width: "220px", height: "0" }}></div>
         <div style={{ width: "220px", height: "0" }}></div>
         <div style={{ width: "220px", height: "0" }}></div>
@@ -34,13 +63,80 @@ export default function Products() {
   );
 }
 
-const Rate = styled.p``;
+const handleBgColor = (color) => {
+  if (color > 8.5) {
+    return "#bc00dc";
+  } else if (color > 7.5) {
+    return "#ff0057";
+  } else if (color > 5.5) {
+    return "#008000";
+  } else {
+    return;
+  }
+};
+
+const Flame = styled(Icons.Flame)`
+  vertical-align: text-bottom;
+  height: 25px;
+  color: #bc00dc;
+  background-color: #000;
+  background-color: rgb(96 125 139 / 0.1);
+  border-radius: 3px;
+  box-shadow: 0px 2px 1px -1px rgba(56, 40, 40, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Check = styled(Icons.Check)`
+  vertical-align: text-bottom;
+  height: 25px;
+  color: #008000;
+  background-color: rgb(96 125 139 / 0.1);
+  border-radius: 3px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Heart = styled(Icons.Heart)`
+  vertical-align: text-bottom;
+  height: 25px;
+  color: #ff0057;
+  background-color: rgb(96 125 139 / 0.1);
+  border-radius: 3px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Rate = styled.p`
+  padding: 5px;
+  font-weight: 500;
+
+  .coloured {
+    color: ${({ color }) => handleBgColor(color)};
+    padding: 5px;
+    background-color: rgb(0 0 0 / 0.07);
+    border-radius: 1000px;
+  }
+
+  &:after {
+    content: "";
+    width: 100%;
+    height: 5px;
+    background-color: ${({ color }) => handleBgColor(color)};
+    display: block;
+    margin-top: -9.5px;
+    z-index: -99;
+    opacity: 0.1;
+    border-radius: 1000px;
+  }
+`;
 
 const Image = styled.img`
   width: 100%;
 `;
 
-const Price = styled.p``;
+const Price = styled.p`
+  padding: 5px;
+`;
 
 const Title = styled.h2``;
 
@@ -53,7 +149,6 @@ const Wrapper = styled.div`
   border-radius: 2px;
   background-color: #fff;
   width: 200px;
-  margin: 2%;
   padding: 10px;
 `;
 
@@ -63,6 +158,5 @@ const Page = styled.div`
   flex-wrap: wrap;
   padding: 3%;
   padding-top: 2%;
-  margin: 0 Auto;
-  gap: 1%;
+  gap: 15px;
 `;
