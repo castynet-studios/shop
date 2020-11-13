ffefae8b81a6f98b0e2434e55273d72cc1d83030
castynet <riungemaina@gmail.com>
2020-11-13 11:09:44 +0300

    App: modified footer position

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from "react";
 import Products from "./components/products";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
-import Quotes from "./components/quotes"
+import Quotes from "./components/quotes";
+import styled from "styled-components";
 // import { useApi } from "./components/productsContext"
 
 function App() {
   return (
     <>
-      <Nav />
-      <Quotes/>
-      <Products />
-      <Footer/>
+      <Page>
+        <Nav />
+        <Quotes />
+        <Products />
+      </Page>
+
+      <Footer />
     </>
   );
 }
+
+const Page = styled.div`
+  min-height: 80vh;
+`;
 
 export default App;
