2f46e035fdf1986da85250c098349844c3605306
castynet <riungemaina@gmail.com>
2020-12-23 17:11:27 +0300

    components/nav: changed box shadow

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -18,7 +18,8 @@ const Nav = styled.nav`
   width: 100%;
   background-color: #203d4b;
   height: 65px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 3px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
 `;
 
 const LogoImg = styled.img`
