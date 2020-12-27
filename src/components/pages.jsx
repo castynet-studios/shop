4b47228b96110e74f185c3fc9d1a2cd386eff9e5
castynet <riungemaina@gmail.com>
2020-12-27 23:17:23 +0300

    components/pages: added render Logic & imported pages md

--- src/components/pages.jsx
+++ src/components/pages.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
+import { Redirect } from "react-router-dom";
 
-export default function () {
+import BuyerScore from "./pages/buyersScore.md";
+import PrivacyPolicy from "./pages/privacyPolicy.md";
+import Ethics from "./pages/ethics.md";
+import Submit from "./pages/submit.md";
+
+export default function (data) {
+  const [file, setFile] = useState();
+  const currentPage = data.match.params.pageSlug;
+
+  useEffect(() => {
+    function PageData() {
+      if (currentPage === "buyers_score") {
+        return BuyerScore;
+      } else if (currentPage === "submit_product") {
+        return Submit;
+      } else if (currentPage === "privacy_policy") {
+        return PrivacyPolicy;
+      } else if (currentPage === "ethics") {
+        return Ethics;
+      } else {
+        return <Redirect to={{ pathname: "/ooh-no" }} />;
+      }
+    }
+    fetch(PageData())
+      .then((res) => res.text())
+      .then((text) => setFile(text));
+  }, [currentPage]);
+
   return (
     <>
-      <p>hello world</p>
+      <Wrapper>
+        <ReactMarkdown source={file} />
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div``;
