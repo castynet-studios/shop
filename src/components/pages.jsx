27ea040a20558f535d97ed0027ae409f6abe887c
castynet <riungemaina@gmail.com>
2020-12-27 19:57:28 +0300

    components/pages: created

---
+++ src/components/pages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+
+export default function () {
+  return (
+    <>
+      <p>hello world</p>
+    </>
+  );
+}
