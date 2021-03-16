2e4b20db5a3e55bc799103e08fe2bb3d49fb3192
castynet <riungemaina@gmail.com>
2021-03-16 16:43:26 +0300

    components/quotes: fixed width on mobile

--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -79,4 +79,5 @@ const QuoteWrap = styled.div`
   border-radius: 3px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  max-width: 90%;
 `;
