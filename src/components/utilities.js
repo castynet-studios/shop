eb58db67868c3cbff88bb9ba53ed07308514f126
castynet <riungemaina@gmail.com>
2021-04-27 07:54:09 +0300

    components/utilities: added nav icons

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -1,31 +1,34 @@
 import { RadioCircleMarked } from "@styled-icons/boxicons-regular/RadioCircleMarked";
 import { ExternalLink } from "@styled-icons/heroicons-outline/ExternalLink";
 import { CheckCircle } from "@styled-icons/boxicons-solid/CheckCircle";
-import { Flame} from "@styled-icons/boxicons-solid/Flame"; 
-import { HeartCircle} from "@styled-icons/boxicons-solid/HeartCircle";
-import { LoaderOutline } from "@styled-icons/evaicons-outline/LoaderOutline"
-
+import { Flame } from "@styled-icons/boxicons-solid/Flame";
+import { HeartCircle } from "@styled-icons/boxicons-solid/HeartCircle";
+import { LoaderOutline } from "@styled-icons/evaicons-outline/LoaderOutline";
+import { NavigationUnread } from "@styled-icons/fluentui-system-filled/NavigationUnread";
+import { Navigation } from "@styled-icons/fluentui-system-filled/Navigation";
 
 export const Icons = {
-    DotCircle: RadioCircleMarked,
-    ExternalLink: ExternalLink,
-    Check: CheckCircle,
-    Flame: Flame,
-    Heart: HeartCircle,
-    Loader: LoaderOutline,
-}
+  DotCircle: RadioCircleMarked,
+  ExternalLink: ExternalLink,
+  Check: CheckCircle,
+  Flame: Flame,
+  Heart: HeartCircle,
+  Loader: LoaderOutline,
+  NavBtnInitial: NavigationUnread,
+  NavBtn: Navigation,
+};
 
 export const Quotes = [
-    "Live for today, plan for tomorrow, party tonight 🎉",
-    "Live to work, work to party, party to live 🎉",
-    "If I stay home one night I start spreading rumours to my dogs 🐶",
-    "Empowering Software ⚡",
-    "The Generation that is tech 🌍",
-    "Treat yo’ self 🎁",
-    "Happiness is not in money, but in shopping 🛒",
-    "Anyone who lives within their means suffers from a lack of imagination 📦",
-    "You can always find something you want 💝",
-    "We all need some good retail therapy 🍀",
-    "Exercise? oh i thought you said accessorize 🎮",
-    "Time moves too fast for an analog watch 🛸",
-]
+  "Live for today, plan for tomorrow, party tonight 🎉",
+  "Live to work, work to party, party to live 🎉",
+  "If I stay home one night I start spreading rumours to my dogs 🐶",
+  "Empowering Software ⚡",
+  "The Generation that is tech 🌍",
+  "Treat yo’ self 🎁",
+  "Happiness is not in money, but in shopping 🛒",
+  "Anyone who lives within their means suffers from a lack of imagination 📦",
+  "You can always find something you want 💝",
+  "We all need some good retail therapy 🍀",
+  "Exercise? oh i thought you said accessorize 🎮",
+  "Time moves too fast for an analog watch 🛸",
+];
