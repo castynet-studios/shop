30fb084c426f7d8be7b4b2858aa0d1f6cfc41ab4
castynet <riungemaina@gmail.com>
2021-06-11 03:35:21 +0300

    components/pagination: added general elements

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import * as Styled from "./styles/pagination";
 
 export default function () {
   return (
     <>
-      <p>hello World</p>
+      <Styled.Wrapper>
+        <Styled.PageWrap>
+          <Styled.Button>1</Styled.Button>
+          ...
+          <Styled.Button>4</Styled.Button>
+          <Styled.Button active>5</Styled.Button>
+          <Styled.Button>6</Styled.Button>
+          ...
+          <Styled.Button>15</Styled.Button>
+        </Styled.PageWrap>
+      </Styled.Wrapper>
     </>
   );
 }
