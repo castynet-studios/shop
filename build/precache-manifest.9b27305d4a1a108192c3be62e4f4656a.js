ce119e3f8b7361c0cc4ccb1dd51b54846adc052d
castynet <riungemaina@gmail.com>
2021-06-15 17:58:04 +0300

    Production v1.4.0 various styling updates

---
+++ build/precache-manifest.9b27305d4a1a108192c3be62e4f4656a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35c7216c0a3238ff6323d280f536376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc5e18634f252fc86c9",
+    "url": "/static/css/main.45146910.chunk.css"
+  },
+  {
+    "revision": "e9ed96b6fd4acf30eedc",
+    "url": "/static/js/2.79ae8e5a.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.79ae8e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc5e18634f252fc86c9",
+    "url": "/static/js/main.1b81609f.chunk.js"
+  },
+  {
+    "revision": "705c69162250a325d3b6",
+    "url": "/static/js/runtime-main.f2e4f4fd.js"
+  },
+  {
+    "revision": "d75d08e28e607c6467f4201db45edc35",
+    "url": "/static/media/buyersScore.d75d08e2.md"
+  },
+  {
+    "revision": "0176d17eb3ddc15c0049c2cb438d65e2",
+    "url": "/static/media/ethics.0176d17e.md"
+  },
+  {
+    "revision": "17c2a1b669e784972610c1bf7576c5a7",
+    "url": "/static/media/genztechShopBackground.17c2a1b6.png"
+  },
+  {
+    "revision": "b9b4b74e3f98fddc4a8bf4181df70431",
+    "url": "/static/media/logo.b9b4b74e.png"
+  },
+  {
+    "revision": "8eef4c00d55cf33e0b5e11dee338a0e4",
+    "url": "/static/media/privacyPolicy.8eef4c00.md"
+  },
+  {
+    "revision": "dd2e002e29da34cb29ef99afc7ee11a0",
+    "url": "/static/media/submit.dd2e002e.md"
+  }
+]);
---
+++ build/static/media/submit.dd2e002e.md
@@ -0,0 +1,7 @@
+# Get a Product Listed
+
+To get your Product listed drop us an email at
+
+[talk-to@castynet.africa](mailto:talk-to@castynet.africa)
+
+with details about the product, images and any relevant reviews.
