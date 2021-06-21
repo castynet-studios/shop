faea4355f20fb83e02fa96f411d3a36a37436374
castynet <riungemaina@gmail.com>
2021-06-21 09:24:32 +0300

    Production v1.4.1 fixed minor build issues

---
+++ build/precache-manifest.d6a91a2b8b5aeef9a00ae75d07df1554.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b83c55b2c689c91dfb0310edde49f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da01967dac61a250e105",
+    "url": "./static/css/main.cdcb5a70.chunk.css"
+  },
+  {
+    "revision": "9f9b16d3b60999b67427",
+    "url": "./static/js/2.23c0c1c5.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "./static/js/2.23c0c1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da01967dac61a250e105",
+    "url": "./static/js/main.516bfba8.chunk.js"
+  },
+  {
+    "revision": "2c68a3733c3f913f7134",
+    "url": "./static/js/runtime-main.4f79a10d.js"
+  },
+  {
+    "revision": "d75d08e28e607c6467f4201db45edc35",
+    "url": "./static/media/buyersScore.d75d08e2.md"
+  },
+  {
+    "revision": "0176d17eb3ddc15c0049c2cb438d65e2",
+    "url": "./static/media/ethics.0176d17e.md"
+  },
+  {
+    "revision": "17c2a1b669e784972610c1bf7576c5a7",
+    "url": "./static/media/genztechShopBackground.17c2a1b6.png"
+  },
+  {
+    "revision": "b9b4b74e3f98fddc4a8bf4181df70431",
+    "url": "./static/media/logo.b9b4b74e.png"
+  },
+  {
+    "revision": "8eef4c00d55cf33e0b5e11dee338a0e4",
+    "url": "./static/media/privacyPolicy.8eef4c00.md"
+  },
+  {
+    "revision": "dd2e002e29da34cb29ef99afc7ee11a0",
+    "url": "./static/media/submit.dd2e002e.md"
+  }
+]);
